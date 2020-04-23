13de303dffb1abf8c19d91cf11441cc070e86b25
kmoon <407578371@qq.com>
2020-04-23 16:23:10 +0800

    457647

--- src/views/User/Index/Index.vue
+++ src/views/User/Index/Index.vue
@@ -1,6 +1,93 @@
 <template>
-  <div>
-    Index视图
+  <div class="main indexPage">
+    <header>
+      <div class="headerCon">
+        <h1>首页</h1>
+      </div>
+    </header>
+    <div class="search">
+      <div class="box">
+        <i class="icon">&#xe628;</i>
+        <input type="text" placeholder="请搜索设备名称、站点及计划时间" class="text" />
+      </div>
+      <button class="icon">&#xe629;</button>
+    </div>
+    <div class="fastNav">
+      <a href="javascript:void(0);">
+        <i class="icon">&#xe61c;</i>
+        <p>园区100家</p>
+      </a>
+      <a href="javascript:void(0);">
+        <i class="icon">&#xe61c;</i>
+        <p>植物10万颗</p>
+      </a>
+      <a href="javascript:void(0);">
+        <i class="icon">&#xe61c;</i>
+        <p>建筑2000个</p>
+      </a>
+    </div>
+    <div class="pageBox">
+      <div class="list">
+        <ul>
+          <li>
+            <a href="javascript:void(0);">
+              <div class="pic">
+                <img src="../../../assets/img/def-pic.png" />
+              </div>
+              <dl>
+                <dt>沙坪坝森林公园</dt>
+                <dd>
+                  <p>集各种古树、教育、文化交流于一体集各种古树、教育、文化交流于一体</p>
+                  <b class="orange">201颗植物</b>
+                </dd>
+              </dl>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);">
+              <div class="pic">
+                <img src="../../../assets/img/def-pic.png" />
+              </div>
+              <dl>
+                <dt>沙坪坝森林公园</dt>
+                <dd>
+                  <p>集各种古树、教育、文化交流于一体集各种古树、教育、文化交流于一体</p>
+                  <b class="orange">201颗植物</b>
+                </dd>
+              </dl>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);">
+              <div class="pic">
+                <img src="../../../assets/img/def-pic.png" />
+              </div>
+              <dl>
+                <dt>沙坪坝森林公园</dt>
+                <dd>
+                  <p>集各种古树、教育、文化交流于一体集各种古树、教育、文化交流于一体</p>
+                  <b class="orange">201颗植物</b>
+                </dd>
+              </dl>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);">
+              <div class="pic">
+                <img src="../../../assets/img/def-pic.png" />
+              </div>
+              <dl>
+                <dt>沙坪坝森林公园</dt>
+                <dd>
+                  <p>集各种古树、教育、文化交流于一体集各种古树、教育、文化交流于一体</p>
+                  <b class="orange">201颗植物</b>
+                </dd>
+              </dl>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/views/User/List/Index.vue
+++ src/views/User/List/Index.vue
@@ -1,6 +1,67 @@
 <template>
-  <div>
-    List视图
+  <div class="main listPage">
+    <div class="search">
+      <div class="box">
+        <i class="icon">&#xe628;</i>
+        <input type="text" placeholder="请搜索设备名称、站点及计划时间" class="text" />
+      </div>
+    </div>
+    <div class="banner">
+      <a href="javascript:void(0);">
+        <img src="../../../assets/img/list-banner.png" />
+      </a>
+    </div>
+    <div class="pageBox">
+      <div class="scrollTxt">
+        <a href="javascript:void(0);">
+          <div class="l">
+            <i class="icon">&#xe625;</i>
+          </div>
+          <div class="c">
+            <p>沙坪坝森林公园创立于2011年。园区拥有3215987万平方米</p>
+          </div>
+          <div class="r">
+            <i class="icon">&#xe61e;</i>
+          </div>
+        </a>
+      </div>
+      <div class="list">
+        <ul>
+          <li>
+            <a href="javascript:void(0);">
+              <div class="pic">
+                <img src="../../../assets/img/sz.jpg" />
+              </div>
+              <p>西北白杨树</p>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);">
+              <div class="pic">
+                <img src="../../../assets/img/list-banner.png" />
+              </div>
+              <p>西北白杨树西北白杨树西北白杨树西北白杨树</p>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);">
+              <div class="pic">
+                <img src="../../../assets/img/sz.jpg" />
+              </div>
+              <p>西北白杨树</p>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0);">
+              <div class="pic">
+                <img src="../../../assets/img/sz.jpg" />
+              </div>
+              <p>西北白杨树</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
