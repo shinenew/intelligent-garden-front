7367aa8e9ca86d52e510c313ba839e53d1b1a6e1
kmoon <407578371@qq.com>
2020-04-24 12:46:49 +0800

    45747

--- src/views/User/PlantDetail/Index.vue
+++ src/views/User/PlantDetail/Index.vue
@@ -7,12 +7,30 @@
       </div>
     </header>
     <div class="bn">
-      <img src="../../../assets/img/list-banner.png" />
+      <div class="swiper-container slider" id="_proSwiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="box">
+              <img
+                src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587388223288&di=2f129d6c99eac03f5ec1d4e3817e89d1&imgtype=0&src=http%3A%2F%2Fa4.att.hudong.com%2F21%2F09%2F01200000026352136359091694357.jpg"
+              />
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="box">
+              <img
+                src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587388223287&di=0b1e046295e5d42730e3c017297cb1d9&imgtype=0&src=http%3A%2F%2Fc.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fd009b3de9c82d1587e249850820a19d8bd3e42a9.jpg"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
     </div>
     <div class="pageBox">
       <div class="detail">
-        <div class="txt">
-          <div class="moreLnk">
+        <div class="txt" id="_txt">
+          <div class="moreLnk" id="_moreTxt">
             <a href="javascript:void(0);">
               查看更多
               <i class="icon">&#xe626;</i>
@@ -32,7 +50,9 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-
+import Swiper from "swiper";
+import "swiper/css/swiper.css";
+import $ from "jquery";
 @Component({
   components: {
     // 视图中用到的组件，在上方import，在这里注入
@@ -40,5 +60,37 @@ import { Component, Vue } from "vue-property-decorator";
 })
 export default class Detail extends Vue {
   // 这里定义属性，方法，prop，emit等，详见vue-property-decorator
+  mounted() {
+    setTimeout(function() {
+      var proSwiper = new Swiper("#_proSwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        loop: true,
+        //simulateTouch : false,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 0,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        },
+        autoplay: {
+          delay: 5000,
+          stopOnLastSlide: false,
+          disableOnInteraction: true
+        }
+      });
+      $("#_moreTxt").on("click", function() {
+        $("#_txt").css("height", "auto");
+        $(this).hide();
+      });
+    }, 500);
+  }
+
+  created() {
+    console.log($("body").width());
+  }
 }
 </script>
